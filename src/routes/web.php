fc4becb12186a2724c3e7b49a1d78fcf1a9fb35b
mariselvanzaigo <mari.s@zaigoinfotech.com>
2025-10-09 13:12:52 +0530

    create user guide

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,8 +2,22 @@
 use ModuleUserGuide\Http\Controllers\ModuleController;
 use ModuleUserGuide\Http\Controllers\UserGuideController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 
 Route::prefix('module-user-guide')->group(function () {
     Route::resource('modules', ModuleController::class);
     Route::resource('user-guides', UserGuideController::class);
 });
+
+
+Route::get('plugin-assets/{path}', function ($path) {
+    $file = base_path('mariselvanzaigo/laravel-user-guide-plugin/moduleuserguide/Resources/assets/' . $path);
+
+    if (!File::exists($file)) {
+        abort(404);
+    }
+
+    $mimeType = File::mimeType($file);
+    return Response::file($file, ['Content-Type' => $mimeType]);
+})->where('path', '.*');
